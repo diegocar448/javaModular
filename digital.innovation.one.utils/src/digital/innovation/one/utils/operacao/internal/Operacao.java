e3835b6836abfbe4cfc2438206a73f6143c40409
Diego Cardoso <diegocar448@hotmail.com>
2021-05-25 13:49:53 -0300

    Java Modular

---
+++ digital.innovation.one.utils/src/digital/innovation/one/utils/operacao/internal/Operacao.java
@@ -0,0 +1,8 @@
+package digital.innovation.one.utils.operacao.internal;
+
+/* Quando a interface for implementada a class se tornará */
+public interface Operacao {
+
+
+    int execute(int a, int b);
+}
